0bc4b729bedc17b9c2b54e998314b8750e200ec3
bcheney21 <brinn_cheney@icloud.comgit config --global user.email brinn_cheney@icloud.com>
2021-04-07 08:59:54 -0700

    bugs reallyy fixed

---
+++ solution.py
@@ -0,0 +1,10 @@
+def sum_squares(nums):
+  total = 0
+  for num in nums:
+    total += num ** 2
+
+  return total
+
+def sum_squares(nums):
+  return sum([num ** 2 for num in nums])
+print(sum_squares([2, 2, 3])) # should be 17
